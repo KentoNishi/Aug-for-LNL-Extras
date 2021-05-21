6cc06043071302df3fefb8e079c2b4652e5e7110
Kento Nishi <kento24gs@outlook.com>
2021-05-20 22:44:21 -0700

    Can load tensors!

--- scripts/train_loss_plot.py
+++ scripts/train_loss_plot.py
@@ -1,3 +1,16 @@
 # %%
 
-# TODO: Write the plotter/animation script
+import torch
+import os
+
+# %%
+LOSS_DIR = "../Augmentation-for-LNL/checkpoints/c10/50sym/AugDesc-WS/loss"
+
+# %%
+
+for filename in os.listdir(LOSS_DIR):
+    if filename.endswith(".pth.tar"):
+        print(f"Loading {filename}...")
+        losses = torch.load(os.path.join(LOSS_DIR, filename))
+        print(f"Loaded {filename}\n")
+
