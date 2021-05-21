ad3f6d47b673207ca2f2c67bc091e4eecf8ac4b6
Kento Nishi <kento24gs@outlook.com>
2021-05-21 09:25:52 -0700

    80sym GIF

--- scripts/train_loss_plot.py
+++ scripts/train_loss_plot.py
@@ -11,12 +11,14 @@ import imageio
 
 
 # %%
-LOSS_DIR = "../Augmentation-for-LNL/checkpoints/c10/50sym/AugDesc-WS/loss"
+LOSS_DIR = "../Augmentation-for-LNL/checkpoints/c10/80sym/AugDesc-WS/loss"
 IS_NOISY_PATH = (
-    "../Augmentation-for-LNL/checkpoints/c10/50sym/AugDesc-WS/saved/is_noisy.npy"
+    "../Augmentation-for-LNL/checkpoints/c10/80sym/AugDesc-WS/saved/is_noisy.npy"
 )
 FRAME_PATH = "./img/train_loss_plot/frames"
-
+N = 100
+SKIP_LOADING = True
+FPS = 10
 
 # %%
 
@@ -48,36 +50,37 @@ def get_epoch(filename):
 
 items = list(filter(lambda f: f.endswith(".pth.tar"), os.listdir(LOSS_DIR)))
 items.sort(key=get_epoch)
-items = items[:20]
+items = items[:N]
 
 fig = plt.figure()
 
-for filename in items:
-    if filename.endswith(".pth.tar"):
-        print(f"Loading {filename}...")
-        losses = torch.load(os.path.join(LOSS_DIR, filename))[0].tolist()
+if not SKIP_LOADING:
+    for filename in items:
+        if filename.endswith(".pth.tar"):
+            print(f"Loading {filename}...")
+            losses = torch.load(os.path.join(LOSS_DIR, filename))[0].tolist()
 
-        clean_items, dirty_items = [], []
-        for i in range(50000):
-            (dirty_items if is_noisy[i] else clean_items).append(losses[i])
+            clean_items, dirty_items = [], []
+            for i in range(50000):
+                (dirty_items if is_noisy[i] else clean_items).append(losses[i])
 
-        print(f"Loaded {filename}\n\n")
+            print(f"Loaded {filename}\n\n")
 
-        sns.set(font_scale=1)
+            sns.set(font_scale=1)
 
-        ax = sns.histplot(
-            create_dataframe(clean_items, dirty_items),
-            x="loss",
-            hue="label",
-            multiple="layer",
-            bins=100,
-            palette=sns.color_palette("tab10")[:2],
-        )
-        epoch = get_epoch(filename)
-        ax.set_title(f"Loss Distribution (50% Sym. Noise, Epoch {epoch})")
+            ax = sns.histplot(
+                create_dataframe(clean_items, dirty_items),
+                x="loss",
+                hue="label",
+                multiple="layer",
+                bins=100,
+                palette=sns.color_palette("tab10")[:2],
+            )
+            epoch = get_epoch(filename)
+            ax.set_title(f"Loss Distribution (80% Sym. Noise, Epoch {epoch})")
 
-        fig.savefig(f"{FRAME_PATH}/epoch{epoch}.png")
-        plt.cla()
+            fig.savefig(f"{FRAME_PATH}/epoch{epoch}.png")
+            plt.cla()
 
 # %%
 
@@ -86,8 +89,11 @@ items = list(filter(lambda f: f.endswith(".png"), os.listdir(FRAME_PATH)))
 items.sort(key=get_epoch)
 for file_name in items:
     if file_name.endswith(".png"):
+        epoch = get_epoch(file_name)
+        if epoch > N:
+            continue
         file_path = os.path.join(FRAME_PATH, file_name)
         images.append(imageio.imread(file_path))
-imageio.mimsave(f"{FRAME_PATH}/../animated.gif", images, fps=3)
+imageio.mimsave(f"{FRAME_PATH}/../animated.gif", images, fps=FPS)
 
 # %%
